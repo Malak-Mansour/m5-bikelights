4b4c82b6a74b7e48e70f10d5ea11ca6c00a03780
Malak-Mansour <73076958+Malak-Mansour@users.noreply.github.com>
2021-05-29 23:49:45 +0400

    Create tasks_123.ino
    trying to use the millis function

---
+++ part 1/tasks_123/tasks_123.ino
@@ -0,0 +1,103 @@
+#include "M5Atom.h"
+
+uint8_t DisBuff[2 + 5 * 5 * 3];
+uint8_t ctr = 0;
+
+void setBuff(uint8_t Rdata, uint8_t Gdata, uint8_t Bdata)
+{
+    DisBuff[0] = 0x05;
+    DisBuff[1] = 0x05;
+    for (int i = 0; i < 25; i++)
+    {
+        DisBuff[2 + i * 3 + 0] = Rdata;
+        DisBuff[2 + i * 3 + 1] = Gdata;
+        DisBuff[2 + i * 3 + 2] = Bdata;
+    }
+}
+
+void setup()
+{
+    M5.begin(true, false, true);
+    delay(10);
+    setBuff(0x00, 0x00, 0x00);
+    M5.dis.displaybuff(DisBuff);
+}
+
+
+void loop()
+{
+    if (M5.Btn.wasPressed())
+    {
+
+        switch (ctr)
+        {
+
+        case 0:
+         {         
+          setBuff(0x00, 0x00, 0x00); //BLACK
+          break;
+          }
+         
+        case 1:        
+         {do
+            {
+            setBuff(0xff, 0xff, 0xff); //WHITE
+                //wait for 1000 ms
+                 int period = 1000;
+                  unsigned long time_now = 0;
+                 // Serial.begin(115200); I think we don't need this line
+                  time_now = millis();
+                  while(millis() < time_now + period){} //wait for 1000 ms
+                  
+            setBuff(0x00, 0x00, 0x00); //BLACK   
+                  //wait for 1000 ms
+                 int period = 1000;
+                  unsigned long time_now = 0;
+                 // Serial.begin(115200); I think we don't need this line
+                  time_now = millis();
+                  while(millis() < time_now + period){} //wait for 1000 ms
+                  
+            } while(!M5.Btn.wasPressed());
+            break;
+         }
+
+         
+        case 2:
+         {do
+            {
+            setBuff(0xff, 0x00, 0x00);//RED
+                //wait for 1000 ms
+                 int period = 1000;
+                  unsigned long time_now = 0;
+                 // Serial.begin(115200); I think we don't need this line
+                  time_now = millis();
+                  while(millis() < time_now + period){} //wait for 1000 ms
+                  
+            setBuff(0x00, 0x00, 0x00); //BLACK   
+                  //wait for 1000 ms
+                 int period = 1000;
+                  unsigned long time_now = 0;
+                 // Serial.begin(115200); I think we don't need this line
+                  time_now = millis();
+                  while(millis() < time_now + period){} //wait for 1000 ms
+                  
+            } while(!M5.Btn.wasPressed());
+            break;
+         }
+
+ 
+        default:
+            break;
+        }
+        M5.dis.displaybuff(DisBuff);
+
+        ctr++;
+        if (ctr >= 3)
+        {
+            ctr = 0;
+        }
+    }
+
+    delay(50);
+    M5.update();
+}
