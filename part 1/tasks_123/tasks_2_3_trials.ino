8aab3dc1eaa8c2f904de1dd5eebfa747ed7fa2a9
Malak-Mansour <73076958+Malak-Mansour@users.noreply.github.com>
2021-05-29 23:50:50 +0400

    Create tasks_2_3_trials.ino
    trying to see if simply stating that it should go from black to white will happen automatically

---
+++ part 1/tasks_123/tasks_2_3_trials.ino
@@ -0,0 +1,85 @@
+#include "M5Atom.h"
+
+uint8_t DisBuff[2 + 5 * 5 * 3];
+uint8_t FSM = 0;
+
+void setBuff(uint8_t Rdata, uint8_t Gdata, uint8_t Bdata)
+{
+    DisBuff[0] = 0x05;
+    DisBuff[1] = 0x05;
+    for (int i = 0; i < 25; i++)
+    {
+        DisBuff[2 + i * 3 + 0] = Rdata;
+        DisBuff[2 + i * 3 + 1] = Gdata;
+        DisBuff[2 + i * 3 + 2] = Bdata;
+    }
+}
+
+void setup()
+{
+    M5.begin(true, false, true);
+    delay(10);
+    setBuff(0x00, 0x00, 0x00);
+    M5.dis.displaybuff(DisBuff);
+   
+}
+
+
+void loop()
+{
+    if (M5.Btn.wasPressed())
+    {
+
+        switch (FSM)
+        {
+
+          case 0:
+          {
+            setBuff(0x00, 0x00, 0x00);
+            break;
+          }
+         
+        case 1:
+         
+         {
+         
+          while (FSM==1)
+         {
+         /* setBuff(0xff, 0xff, 0xff); //#define WHITE           rgb(0xFF,0xFF,0xFF)
+              delay (1000);
+              setBuff(0x00, 0x00, 0x00);    
+              delay (1000);                    
+             */
+             setBuff(0x00, 0x00, 0x00); // off state
+            M5.dis.displaybuff(DisBuff); // display the state
+            delay(1000); // delay the off state for 0.5 second
+            setBuff(0xff, 0xff, 0xff); // set state of color to white
+            M5.dis.displaybuff(DisBuff); // display the state
+            delay(1000);// delay for 0.5 second
+            M5.update(); // update the state of the board
+         }
+       //     break;
+     
+         }
+        case 2:
+       
+        {
+          setBuff(0xff, 0x00, 0x00);//#define RED rgb(0xFF,0x00,0x00
+          break;
+        }
+ 
+        default:
+            break;
+        }
+        M5.dis.displaybuff(DisBuff);
+
+        FSM++;
+        if (FSM >= 3)
+        {
+            FSM = 0;
+        }
+    }
+
+    delay(50);
+    M5.update();
+}
