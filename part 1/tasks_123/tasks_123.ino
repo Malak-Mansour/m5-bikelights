2cb1b70c6ac239a0b3e583836d0575344d047c83
Malak-Mansour <73076958+Malak-Mansour@users.noreply.github.com>
2021-05-30 14:01:41 +0400

    Update tasks_123.ino
    changed to fillpix and reduced the strobing intervals

--- part 1/tasks_123/tasks_123.ino
+++ part 1/tasks_123/tasks_123.ino
@@ -1,30 +1,14 @@
 #include "M5Atom.h"
 
-uint8_t DisBuff[2 + 5 * 5 * 3];
 uint8_t FSM = 0;
 unsigned long previousTime=0; 
 bool state=false;
 unsigned long currentTime;
 
-void setBuff(uint8_t Rdata, uint8_t Gdata, uint8_t Bdata)
-{
-    DisBuff[0] = 0x05;
-    DisBuff[1] = 0x05;
-    for (int i = 0; i < 25; i++)
-    {
-        DisBuff[2 + i * 3 + 0] = Rdata;
-        DisBuff[2 + i * 3 + 1] = Gdata;
-        DisBuff[2 + i * 3 + 2] = Bdata;
-    }
-}
-
 
 void setup()
 {
-    M5.begin(true, false, true);
-   setBuff(0x00, 0x00, 0x00);
-   M5.dis.displaybuff(DisBuff);
-   //M5.dis.drawpix(i,0x000000);//black
+   M5.begin(true, false, true);
 }
 
 
@@ -32,7 +16,7 @@ void setup()
 void loop()
 {
     currentTime=millis(); //millis retrieves current time and stores it into variable currentTime
-     if(currentTime-previousTime>=1000){ //if 1 second passed, switch the state 
+     if(currentTime-previousTime>=50){ //if 50 milliseconds passed, switch the state 
             state=!state;
             previousTime=currentTime;
      }
@@ -45,22 +29,20 @@ void loop()
         {
 
         case 0:
-         {         
-         // M5.dis.drawpix(i,0x000000); //BLACK
+         {     
+          M5.dis.fillpix(0x000000);//black
           break;
           }
          
         case 1:        
          {
               if(state==false){
-               // M5.dis.drawpix(i,0x00ff00); //red
-                setBuff(0xff, 0x00, 0x00); //red
+                  M5.dis.fillpix(0xff0000);//red
               }
               else {
-              //  M5.dis.drawpix(i,0x000000);//black
-               setBuff(0x00, 0x00, 0x00); //black
+                 M5.dis.fillpix(0x000000);//black
               }
-            }
+            
             break;
         }
 
@@ -68,14 +50,12 @@ void loop()
         case 2:
         {
             if(state==false){
-               // M5.dis.drawpix(i,0xffffff); //white
-               setBuff(0xff, 0xff, 0xff); //white
+                 M5.dis.fillpix(0xffffff);//white
               }
               else {
-               // M5.dis.drawpix(i,0x000000);//black
-               setBuff(0x00, 0x00, 0x00); //black
+                M5.dis.fillpix(0x000000);//black
               }
-            }
+           
             break;
         }
 
@@ -83,7 +63,6 @@ void loop()
         default:
             break;
         }
-        M5.dis.displaybuff(DisBuff);
 
         FSM++;
         if (FSM >= 3)
