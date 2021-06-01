31e252b7fbd6ee03c7af47bb66c5e3f0d7a1ccdf
Malak-Mansour <73076958+Malak-Mansour@users.noreply.github.com>
2021-06-02 00:31:21 +0400

    Create temp_sensor_accelerometer.ino
    we fixed the temperature recording that happens at a predetermined interval (5 mins)

---
+++ temperature_sensor/temp_sensor_accelerometer/temp_sensor_accelerometer.ino
@@ -0,0 +1,289 @@
+/*make use of: https://github.com/m5stack/M5Atom
+   https://github.com/m5stack/m5-docs/blob/master/docs/en/api/lcd.md
+  void drawpix(uint8_t xpos, uint8_t ypos, CRGB Color): Set the color of a LED in coordinate mode
+  void displaybuff(uint8_t *1.c, int8_t offsetx = 0, int8_t offsety = 0): Display an image
+  https://github.com/lukasmaximus89/M5Atom-Resources/blob/master/glcdfont.c for font
+  M5.IMU.getAccelData(&accX, &accY, &accZ); to get x,y,&z axis rotations
+   M5.IMU.getAhrsData(&pitch, &roll, &yaw); to get attitude along all axes
+*/
+
+#include <I2C_MPU6886.h>
+
+#include "M5Atom.h"
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
+
+
+
+#ifndef PSTR
+#define PSTR // Make Arduino Due happy
+#endif
+#define PIN 27
+
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(5, 5, PIN,
+                            NEO_MATRIX_TOP     + NEO_MATRIX_RIGHT +
+                            NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE,
+                            NEO_GRB            + NEO_KHZ800);
+
+const uint16_t colors[] = {
+  matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255)
+};
+
+
+
+
+float t = 0, t_f = 0, t_k = 0; //temporary variable to store current temperatures
+uint8_t FSM = 0;
+unsigned long previousTime = 0;
+bool state = false;
+int n = 144; //array size, 24hrs  milliseconds
+float accX = 0, accY = 0, accZ = 0;
+bool IMU6886Flag = false;
+
+//array to store temperature values from the last 24 hours (one value each 5 mins)
+unsigned long temp24h[144] = {0};
+int ctr = 0; //for array indeces
+unsigned long sum = 0; //for sum of array elements
+unsigned long avgTemp24 = 0;
+
+
+void setup()
+{
+  Serial.begin(115200);
+  M5.begin(true, true, true);
+  M5.IMU.Init();
+
+  matrix.begin();
+  matrix.setTextWrap(false);
+  matrix.setBrightness(15);
+  matrix.setTextColor(colors[0]);
+
+
+
+  do {
+    int x = 1;
+  } while (accZ >= 0 || !M5.Btn.wasPressed()); //facing upwards and button is pressed (wakeup)
+
+  //both have to be true to break out of this loop, then set FSM to the first case
+  FSM = 0;
+
+}
+
+
+int x = matrix.width();
+int pass = 0;
+
+
+void loop()
+{
+
+
+  M5.IMU.getTempData(&t);
+
+
+  M5.IMU.getAccelData(&accX, &accY, &accZ);
+
+
+  //millis retrieves currentc time
+  //if 5 minutes passed, get the temp and store in array
+  if (millis() - previousTime >= 5 * 60 * 1000) {
+    M5.IMU.getTempData(&t);
+    temp24h[ctr] = t;
+
+    //only increment while array is not full yet
+    if (ctr < (n - 1)) {
+      ctr++;
+    }
+
+    //once all are filled, shift each value in array 1 unit to the left
+    else if (ctr >= (n - 1)) {
+
+      for (int j = 0; j < (n-1); j++) {
+        temp24h[j] = temp24h[j + 1];
+      }
+
+    }
+  }
+
+
+
+  if (IMU6886Flag == true)
+  {
+
+
+    //condition for not sleeping
+
+    if (accX > 0) { //titled to the right
+      FSM++;
+      if (FSM > 5) {
+        FSM == 1;
+      }
+    }
+
+    else if (accX < 0) { //titled to the left
+      FSM--;
+      if (FSM < 1) {
+        FSM == 5;
+      }
+    }
+
+    else if (accZ > 0) {
+      FSM = 0; //facing down, shut it down
+      if (accZ < 0 && M5.Btn.wasPressed() ) //facing upwards
+        FSM = 1; //NOT SURE
+    }
+
+
+
+    switch (FSM)
+    {
+
+      case 0: //off (sleeping mode)
+        {
+          M5.dis.fillpix(0x000000);//black
+          break;
+        }
+
+      case 1: //display current temp
+        {
+          // void displaybuff(uint8_t * 1.c, int8_t offsetx = 0, int8_t offsety = 0)
+
+          //Display image of 1
+
+          if (M5.Btn.wasPressed())
+          {
+            M5.IMU.getTempData(&t);
+
+            //ALTERNATIVELY REPLACE *C WITH IMAGE I MADE!!
+            // matrix.print(F(t + "*C"));
+          }
+          break;
+        }
+
+      case 2: //calculate avg temp
+        {
+          //display image 2
+          if (M5.Btn.wasPressed())
+          {
+            sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+              sum += temp24h[i];
+            }
+            avgTemp24 = sum / n;
+
+            matrix.fillScreen(0);
+            matrix.setCursor(x, 0);
+
+            //ALTERNATIVELY REPLACE *C WITH IMAGE I MADE!!
+            matrix.print(F(avgTemp24 + "*C"));
+            if (--x < -25) {
+              x = matrix.width();
+              if (++pass >= 4) pass = 0;
+              matrix.setTextColor(colors[pass]);
+            }
+          }
+          break;
+        }
+
+
+      case 3: //color scale
+        {
+          //display image 3
+          if (M5.Btn.wasPressed())
+          {
+            //blue
+            M5.dis.drawpix(1, 0x0000ff);
+            M5.dis.drawpix(6, 0x0000ff);
+            //green
+            M5.dis.drawpix(2, 0x00ff00);
+            M5.dis.drawpix(7, 0x00ff00);
+            //yellow
+            M5.dis.drawpix(3, 0xf1c40f);
+            M5.dis.drawpix(8, 0xf1c40f);
+            //orange
+            M5.dis.drawpix(4, 0xd35400);
+            M5.dis.drawpix(9, 0xd35400);
+            //red
+            M5.dis.drawpix(5, 0xff0000);
+            M5.dis.drawpix(10, 0xff0000);
+
+            if (t < 15) {   //display blue
+              M5.dis.drawpix(11, 0x0000ff); M5.dis.drawpix(12, 0x0000ff); M5.dis.drawpix(13, 0x0000ff); M5.dis.drawpix(14, 0x0000ff); M5.dis.drawpix(15, 0x0000ff);
+              M5.dis.drawpix(16, 0x0000ff); M5.dis.drawpix(17, 0x0000ff); M5.dis.drawpix(18, 0x0000ff); M5.dis.drawpix(19, 0x0000ff); M5.dis.drawpix(20, 0x0000ff);
+              M5.dis.drawpix(21, 0x0000ff); M5.dis.drawpix(22, 0x0000ff); M5.dis.drawpix(23, 0x0000ff); M5.dis.drawpix(24, 0x0000ff); M5.dis.drawpix(25, 0x0000ff);
+            }
+
+            else if (t >= 15 && t < 22) //display green
+            {
+              M5.dis.drawpix(11, 0x00ff00); M5.dis.drawpix(12, 0x00ff00); M5.dis.drawpix(13, 0x00ff00); M5.dis.drawpix(14, 0x00ff00); M5.dis.drawpix(15, 0x00ff00);
+              M5.dis.drawpix(16, 0x00ff00); M5.dis.drawpix(17, 0x00ff00); M5.dis.drawpix(18, 0x00ff00); M5.dis.drawpix(19, 0x00ff00); M5.dis.drawpix(20, 0x00ff00);
+              M5.dis.drawpix(21, 0x00ff00); M5.dis.drawpix(22, 0x00ff00); M5.dis.drawpix(23, 0x00ff00); M5.dis.drawpix(24, 0x00ff00); M5.dis.drawpix(25, 0x00ff00);
+            }
+            else if (t >= 22 && t < 35) //display yellow
+            {
+              M5.dis.drawpix(11, 0xf1c40f); M5.dis.drawpix(12, 0xf1c40f); M5.dis.drawpix(13, 0xf1c40f); M5.dis.drawpix(14, 0xf1c40f); M5.dis.drawpix(15, 0xf1c40f);
+              M5.dis.drawpix(16, 0xf1c40f); M5.dis.drawpix(17, 0xf1c40f); M5.dis.drawpix(18, 0xf1c40f); M5.dis.drawpix(19, 0xf1c40f); M5.dis.drawpix(20, 0xf1c40f);
+              M5.dis.drawpix(21, 0xf1c40f); M5.dis.drawpix(22, 0xf1c40f); M5.dis.drawpix(23, 0xf1c40f); M5.dis.drawpix(24, 0xf1c40f); M5.dis.drawpix(25, 0xf1c40f);
+            }
+            else if (t >= 35 && t < 39) //display orange
+            {
+              M5.dis.drawpix(11, 0xd35400); M5.dis.drawpix(12, 0xd35400); M5.dis.drawpix(13, 0xd35400); M5.dis.drawpix(14, 0xd35400); M5.dis.drawpix(15, 0xd35400);
+              M5.dis.drawpix(16, 0xd35400); M5.dis.drawpix(17, 0xd35400); M5.dis.drawpix(18, 0xd35400); M5.dis.drawpix(19, 0xd35400); M5.dis.drawpix(20, 0xd35400);
+              M5.dis.drawpix(21, 0xd35400); M5.dis.drawpix(22, 0xd35400); M5.dis.drawpix(23, 0xd35400); M5.dis.drawpix(24, 0xd35400); M5.dis.drawpix(25, 0xd35400);
+            }
+            else if (t >= 15 && t < 22) //display red
+            {
+              M5.dis.drawpix(11, 0xff0000); M5.dis.drawpix(12, 0xff0000); M5.dis.drawpix(13, 0xff0000); M5.dis.drawpix(14, 0xff0000); M5.dis.drawpix(15, 0xff0000);
+              M5.dis.drawpix(16, 0xff0000); M5.dis.drawpix(17, 0xff0000); M5.dis.drawpix(18, 0xff0000); M5.dis.drawpix(19, 0xff0000); M5.dis.drawpix(20, 0xff0000);
+              M5.dis.drawpix(21, 0xff0000); M5.dis.drawpix(22, 0xff0000); M5.dis.drawpix(23, 0xff0000); M5.dis.drawpix(24, 0xff0000); M5.dis.drawpix(25, 0xff0000);
+            }
+          }
+          break;
+        }
+
+
+      case 4: //graphing
+        {
+
+          //display image 4
+          if (M5.Btn.wasPressed())
+          {
+            //check function for graphing
+            matrix.print(F("GRAPH"));
+          }
+          break;
+
+        }
+
+      case 5: //change units
+        {
+          //display image 5
+          if (M5.Btn.wasPressed())
+          {
+            M5.IMU.getTempData(&t);
+
+            t_k = t + 273;
+            //ALTERNATIVELY REPLACE K WITH IMAGE I MADE!!
+            matrix.print(F(t_k + "K"));
+
+            t_f = (t * (9 / 5)) + 32;
+            //ALTERNATIVELY REPLACE *F WITH IMAGE I MADE!!
+            matrix.print(F(t_f + "*F"));
+          }
+          break;
+        }
+
+
+      default:
+        break;
+    }
+
+
+    matrix.show();
+
+    M5.update();
+  }
+}
