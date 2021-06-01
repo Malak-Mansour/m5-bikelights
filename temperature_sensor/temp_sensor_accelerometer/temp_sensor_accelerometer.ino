e2ca6b4be9907430a98022a6a5bd0296e360488c
Malak-Mansour <73076958+Malak-Mansour@users.noreply.github.com>
2021-06-02 02:00:55 +0400

    Update temp_sensor_accelerometer.ino
    tried to allow for sleeping thru cases, so once it "sleeps" go to mode 0 and repeated that at the beginning of all cases

--- temperature_sensor/temp_sensor_accelerometer/temp_sensor_accelerometer.ino
+++ temperature_sensor/temp_sensor_accelerometer/temp_sensor_accelerometer.ino
@@ -5,6 +5,8 @@
   https://github.com/lukasmaximus89/M5Atom-Resources/blob/master/glcdfont.c for font
   M5.IMU.getAccelData(&accX, &accY, &accZ); to get x,y,&z axis rotations
    M5.IMU.getAhrsData(&pitch, &roll, &yaw); to get attitude along all axes
+  // void displaybuff(uint8_t * 1.c, int8_t offsetx = 0, int8_t offsety = 0)
+
 */
 
 #include <I2C_MPU6886.h>
@@ -32,13 +34,11 @@ const uint16_t colors[] = {
 };
 
 
-
-
 float t = 0, t_f = 0, t_k = 0; //temporary variable to store current temperatures
 uint8_t FSM = 0;
 unsigned long previousTime = 0;
 bool state = false;
-int n = 288; //array size, 24hrs  milliseconds
+int n = 288; //array size, (24hrs*60mins)/5mins: our predetermined interval
 float accX = 0, accY = 0, accZ = 0;
 bool IMU6886Flag = false;
 
@@ -48,6 +48,7 @@ int ctr = 0; //for array indeces
 unsigned long sum = 0; //for sum of array elements
 unsigned long avgTemp24 = 0;
 int count = 0;
+
 
 void setup()
 {
@@ -59,15 +60,6 @@ void setup()
   matrix.setTextWrap(false);
   matrix.setBrightness(15);
   matrix.setTextColor(colors[0]);
-
-
-
-  do {
-    int y = 1;
-  } while (accZ >= 0 || !M5.Btn.wasPressed()); //facing upwards and button is pressed (wakeup)
-
-  //both have to be true to break out of this loop, then set FSM to the first case
-  FSM = 0;
 
 }
 
@@ -113,7 +105,7 @@ void loop()
   {
 
 
-    //condition for not sleeping
+    //condition for sleeping
     if (accZ > 0 && !M5.Btn.wasPressed()) {  //sleep, face down
       M5.dis.fillpix(0x000000);//black
     }
@@ -134,11 +126,8 @@ void loop()
         }
       }
 
-      else if (accZ > 0) { //facing down, shut it down
-        FSM = 0;
-
-        if (accZ < 0 && M5.Btn.wasPressed() ) //facing upwards
-          FSM = 1; //NOT SURE
+      if (accZ > 0) { //facing down, nothing displayed
+        FSM = 0; //placed it here so that if titlting was detected but it was facing down, it resets everything back to 0
       }
 
 
@@ -149,139 +138,144 @@ void loop()
           {
             M5.dis.fillpix(0x000000);//black
 
-            if (accZ > 0) { //facing up
-              if (accX > 0) { //titled to the right
+            break;
+          }
 
-              }
-              
+        case 1: //display current temp
+        { if (accZ > 0) { //facing down, nothing displayed
+              FSM = 0; //placed it here so that if titlting was detected but it was facing down, it resets everything back to 0
+              break; //AS IN BREAK OUT OF THIS CASE, IDK IF ITS PLACED RIGHT THO
             }
+
+            //Display image of 1
+
+            if (M5.Btn.wasPressed())
+            {
+              M5.IMU.getTempData(&t);
+
+              //ALTERNATIVELY REPLACE *C WITH IMAGE I MADE!!
+              // matrix.print(F(t + "*C"));
+            }
+            break;
+          }
+
+        case 2: //calculate avg temp
+        { if (accZ > 0) { //facing down, nothing displayed
+              FSM = 0; //placed it here so that if titlting was detected but it was facing down, it resets everything back to 0
+              break; //AS IN BREAK OUT OF THIS CASE, IDK IF ITS PLACED RIGHT THO
+            }
+            //display image 2
+
+            if (M5.Btn.wasPressed())
+            {
+              sum = 0;
+              count = 0;
+
+              for (int i = 0; i < n; i++)
+              {
+                if (temp24h[i] != 0) {
+                  sum += temp24h[i];
+                  count++;
+                }
+              }
+
+              avgTemp24 = sum / count;
+
+              //display avgTemp24
+              //ALTERNATIVELY REPLACE *C WITH IMAGE I MADE!!
+            }
+            break;
+          }
+
+
+        case 3: //color scale
+        { if (accZ > 0) { //facing down, nothing displayed
+              FSM = 0; //placed it here so that if titlting was detected but it was facing down, it resets everything back to 0
+              break; //AS IN BREAK OUT OF THIS CASE, IDK IF ITS PLACED RIGHT THO
+            }
+            //display image 3
+            if (M5.Btn.wasPressed())
+            {
+
+              if (t < 15)
+                M5.dis.fillpix(0x0000ff);//blue
+              else if (t >= 15 && t < 22)
+                M5.dis.fillpix(0x00ff00);//green
+              else if (t >= 22 && t < 35)
+                M5.dis.fillpix(0xf1c40f);//yellow
+              else if (t >= 35 && t < 39)
+                M5.dis.fillpix(0xd35400);//orange
+              else if (t >= 15 && t < 22)
+                M5.dis.fillpix(0xff0000);//red
+
+
+              //color scale
+              //blue
+              M5.dis.drawpix(1, 0x0000ff);
+              M5.dis.drawpix(6, 0x0000ff);
+              //green
+              M5.dis.drawpix(2, 0x00ff00);
+              M5.dis.drawpix(7, 0x00ff00);
+              //yellow
+              M5.dis.drawpix(3, 0xf1c40f);
+              M5.dis.drawpix(8, 0xf1c40f);
+              //orange
+              M5.dis.drawpix(4, 0xd35400);
+              M5.dis.drawpix(9, 0xd35400);
+              //red
+              M5.dis.drawpix(5, 0xff0000);
+              M5.dis.drawpix(10, 0xff0000);
+
+
+            }
+            break;
+          }
+
+
+        case 4: //graphing
+        { if (accZ > 0) { //facing down, nothing displayed
+              FSM = 0; //placed it here so that if titlting was detected but it was facing down, it resets everything back to 0
+              break; //AS IN BREAK OUT OF THIS CASE, IDK IF ITS PLACED RIGHT THO
+            }
+            //display image 4
+            if (M5.Btn.wasPressed())
+            {
+              //check function for graphing
+            }
+            break;
 
           }
 
+
+        case 5: //change units
+        { if (accZ > 0) { //facing down, nothing displayed
+              FSM = 0; //placed it here so that if titlting was detected but it was facing down, it resets everything back to 0
+              break; //AS IN BREAK OUT OF THIS CASE, IDK IF ITS PLACED RIGHT THO
+            }
+            //display image 5
+            if (M5.Btn.wasPressed())
+            {
+              M5.IMU.getTempData(&t);
+
+              t_k = t + 273;
+              //display t_k
+              //ALTERNATIVELY REPLACE K WITH IMAGE I MADE!!
+
+              t_f = (t * (9 / 5)) + 32;
+              //display t_f
+              //ALTERNATIVELY REPLACE *F WITH IMAGE I MADE!!
+            }
+            break;
+          }
+
+
+        default:
           break;
       }
 
-    case 1: //display current temp
-      {
-        // void displaybuff(uint8_t * 1.c, int8_t offsetx = 0, int8_t offsety = 0)
 
-        //Display image of 1
+      matrix.show();
 
-        if (M5.Btn.wasPressed())
-        {
-          M5.IMU.getTempData(&t);
-
-          //ALTERNATIVELY REPLACE *C WITH IMAGE I MADE!!
-          // matrix.print(F(t + "*C"));
-        }
-        break;
-      }
-
-    case 2: //calculate avg temp
-      {
-        //display image 2
-
-        if (M5.Btn.wasPressed())
-        {
-          sum = 0;
-          count = 0;
-
-          for (int i = 0; i < n; i++)
-          {
-            if (temp24h[i] != 0) {
-              sum += temp24h[i];
-              count++;
-            }
-          }
-
-          avgTemp24 = sum / count;
-
-          //display avgTemp24
-          //ALTERNATIVELY REPLACE *C WITH IMAGE I MADE!!
-        }
-        break;
-      }
-
-
-    case 3: //color scale
-      {
-        //display image 3
-        if (M5.Btn.wasPressed())
-        {
-
-          if (t < 15)
-            M5.dis.fillpix(0x0000ff);//blue
-          else if (t >= 15 && t < 22)
-            M5.dis.fillpix(0x00ff00);//green
-          else if (t >= 22 && t < 35)
-            M5.dis.fillpix(0xf1c40f);//yellow
-          else if (t >= 35 && t < 39)
-            M5.dis.fillpix(0xd35400);//orange
-          else if (t >= 15 && t < 22)
-            M5.dis.fillpix(0xff0000);//red
-
-
-          //color scale
-          //blue
-          M5.dis.drawpix(1, 0x0000ff);
-          M5.dis.drawpix(6, 0x0000ff);
-          //green
-          M5.dis.drawpix(2, 0x00ff00);
-          M5.dis.drawpix(7, 0x00ff00);
-          //yellow
-          M5.dis.drawpix(3, 0xf1c40f);
-          M5.dis.drawpix(8, 0xf1c40f);
-          //orange
-          M5.dis.drawpix(4, 0xd35400);
-          M5.dis.drawpix(9, 0xd35400);
-          //red
-          M5.dis.drawpix(5, 0xff0000);
-          M5.dis.drawpix(10, 0xff0000);
-
-
-        }
-        break;
-      }
-
-
-    case 4: //graphing
-      {
-        //display image 4
-        if (M5.Btn.wasPressed())
-        {
-          //check function for graphing
-        }
-        break;
-
-      }
-
-
-    case 5: //change units
-      {
-        //display image 5
-        if (M5.Btn.wasPressed())
-        {
-          M5.IMU.getTempData(&t);
-
-          t_k = t + 273;
-          //display t_k
-          //ALTERNATIVELY REPLACE K WITH IMAGE I MADE!!
-
-          t_f = (t * (9 / 5)) + 32;
-          //display t_f
-          //ALTERNATIVELY REPLACE *F WITH IMAGE I MADE!!
-        }
-        break;
-      }
-
-
-    default:
-      break;
+      M5.update();
     }
-
-
-    matrix.show();
-
-    M5.update();
   }
-}
